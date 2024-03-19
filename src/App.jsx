6cfe46dd842ef183893147572b4d0a50f5b280e6
Diyora <157114574+diyoran@users.noreply.github.com>
2024-03-19 16:33:05 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ const App = () => {
     <Fragment>
       <main className="main">
         <Routes>
-          <Route path="/Market" index element={<Home />} />
-          <Route path="/Market/product/:id" element={<Product />} />
+          <Route path="/Market-main" index element={<Home />} />
+          <Route path="/Market-main/product/:id" element={<Product />} />
           <Route path="*" element={<h2 className="title-2">404</h2>} />
-          <Route path="/Market/bag" element={<Bag />} />
+          <Route path="/Market-main/bag" element={<Bag />} />
         </Routes>
       </main>
     </Fragment>
